e4e210bc069d2dc4bc881202b69b385b3be4f61b
Ashutosh Kumar <corp.ekosys@gmail.com>
2026-06-25 11:24:21 +0530

    Sort Array

--- lecture29_Array/Sort0and1.java
+++ lecture29_Array/Sort0and1.java
@@ -1,4 +1,41 @@
 public class Sort0and1{
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 1, 0, 1, 0, 0, 1, 0, 1};
+
+        int first = 0;
+        int last = arr.length-1;
+        
+        while(first<last){
+            if(arr[first] == 0 && arr[last] == 1){
+                first++;
+                last--;
+            }
+            else if(arr[first] ==1 && arr[last] == 0){
+                int temp = arr[first];
+                arr[first] = arr[last];
+                arr[last] = temp;
+                first++;
+                last--;
+            }
+            else if(arr[first] == 0 && arr[last] == 0){
+                first++;
+            }
+            else if(arr[first]==1 && arr[last] == 1){
+                last--;
+            }
+        }
+
+        for(int i = 0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
+
+
+/*
     public static void main(String[] args) {
         int[] arr = {1, 0, 1, 0, 1, 0, 0, 1, 0, 1};
 
@@ -19,4 +56,8 @@ public class Sort0and1{
         }
         
     }
+
+ */
+
+
 }
